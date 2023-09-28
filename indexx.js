8dabbc717678c35786a6061c278d8e66f9cc7e3a
QuocScript <falwoefh1@gmail.com>
2023-09-28 19:37:18 +0700

    aasv;oausbdvliasbv

--- indexx.js
+++ indexx.js
@@ -1,9 +1,1 @@
 // test file ahahahhahah
-
-// git init : tạo một repository (dự án trên local)
-// git status : check xem có file nào chưa được add vào
-// git add. : add tất cả cái file, folder vào repository
-// git commit -m "note"
-// remote add origin https://github.com/QuocScript/test3.git
-// git branch -M main : tạo ra một branch(nhánh) mới
-// git push -u origin main : đẩy các file/folder đã add lên github.com
